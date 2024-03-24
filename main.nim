db517b5acf4da8297d6199f3fe8729aa0e76fbc9
pattmayne <pattmayne@gmail.com>
2024-03-24 12:36:07 -0400

    completely works

--- main.nim
+++ main.nim
@@ -1,5 +1,9 @@
 import std/strutils
+import std/random
+import std/math
 
+# Call randomize() once to initialize the default random number generator.
+randomize()
 
 proc getNumberFromUser(minInput: int8, maxInput: int8, question: string): int8 =
     let isMaxBigger = maxInput > minInput
@@ -35,7 +39,6 @@ let rolls: int8 = getNumberFromUser(1, 20,
         "How many times do you want to roll it? ")
 echo "You chose to roll the ", sides, " sided die ", rolls, " times."
 
-
 let highestPossibleSum: int = rolls * sides
 let middleNumber = int(((highestPossibleSum - rolls) / 2)) + int(rolls)
 
@@ -43,12 +46,30 @@ echo "The lowest possible sum is: ", rolls
 echo "The middle possible sum is: ", middleNumber
 echo "The highest possible sum is: ", highestPossibleSum
 
+# DO THE ROLL
 
+var sum: int = 0
+
+for i in 1 .. rolls:
+    let roll = rand(sides)
+    #echo "Roll ", i, " is a ", roll
+    sum += roll
+
+echo "Total of all rolls is ", sum
+
+let absoluteDistanceFromCenter: int =
+    if sum >= middleNumber: sum - middleNumber
+        else: middleNumber - sum
+
+echo "Absolute distance from center is ", absoluteDistanceFromCenter
+
+let percentageFromCenter: int =
+    int((absoluteDistanceFromCenter / highestPossibleSum) * 100)
+
+echo "Percentage from center is ", percentageFromCenter
 #[
-    Now we have our numbers
-    Time to calculate the middle RANGE
     Then do the roll
-    and see how far off we were
+    and see how far off we were from middle possibility
     both by raw numbers and percentages
 
 ]#
