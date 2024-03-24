407a2106c8ddcfb7805811d00f53011cdf0b31bb
pattmayne <pattmayne@gmail.com>
2024-03-24 03:01:50 -0400

    function works

--- main.nim
+++ main.nim
@@ -14,7 +14,10 @@ let rolls: int = parseInt(readLine(stdin))
 echo "You chose to roll the ", sides, " sided die ", rolls, " times."
 
 
-proc getNumberFromUser(min: int8, max: int8, question: string): int8 =
+proc getNumberFromUser(minInput: int8, maxInput: int8, question: string): int8 =
+    let isMaxBigger = maxInput > minInput
+    let min: int8 = if isMaxBigger: minInput else: maxInput
+    let max: int8 = if isMaxBigger: maxInput else: minInput
     # These will store the chosen number after the checks
     var chosenNumber: int8 = min - 1
     var chosenNumberString = chosenNumber
@@ -29,9 +32,8 @@ proc getNumberFromUser(min: int8, max: int8, question: string): int8 =
                 chosenNumber = int8(chosenNumberString)
                 return chosenNumber
             elif i >= max:
-                echo "Input out of range. Please choose again. "
+                echo "Input out of range. Choose between ", min, " and ", max
+    chosenNumber
 
-    return chosenNumber
-
-let newSide: int8 = getNumberFromUser(5, 7, "How many newside? ")
+let newSide: int8 = getNumberFromUser(5, 14, "How many newside? ")
 echo "You chose ", newSide
