448f38e4e872da2d2020b5d0ba0d110c10893d56
pattmayne <pattmayne@gmail.com>
2024-03-24 02:52:42 -0400

    works

--- main.nim
+++ main.nim
@@ -14,8 +14,24 @@ let rolls: int = parseInt(readLine(stdin))
 echo "You chose to roll the ", sides, " sided die ", rolls, " times."
 
 
-proc getNumberFromUser(min: int8, max: int8): int8 = 
-	var chosenNumber: int8 = min - 1
-	while chosenNumber < min:
-		echo "How many-sided die? (2 to 20) "
-	return chosenNumber
+proc getNumberFromUser(min: int8, max: int8, question: string): int8 =
+    # These will store the chosen number after the checks
+    var chosenNumber: int8 = min - 1
+    var chosenNumberString = chosenNumber
+
+    # check input against range of allowed inputs
+    while chosenNumber < min:
+        echo question
+        var userInput = readLine(stdin)
+        for i in min .. max:
+            if userInput == intToStr(i):
+                chosenNumberString = i
+                chosenNumber = int8(chosenNumberString)
+                return chosenNumber
+            elif i >= max:
+                echo "Input out of range. Please choose again. "
+
+    return chosenNumber
+
+let newSide: int8 = getNumberFromUser(5, 7, "How many newside? ")
+echo "You chose ", newSide
